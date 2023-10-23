826cfe172bbc05851715b536580230f88231d4c7
iraola <eduiraola@gmail.com>
2023-10-23 15:14:25 +0200

    Separate database init from writes

--- components/server/src/main/java/es/bsc/server/Server.java
+++ components/server/src/main/java/es/bsc/server/Server.java
@@ -18,24 +18,31 @@ import java.util.concurrent.TimeoutException;
 public class Server implements AutoCloseable{
     private final Connection connection;
     private Channel channel;
+    private InfluxDB influxDB;
     private final String EXCHANGE_NAME = "measurements";
+    private final String serverURL = "http://127.0.0.1:8086";
+    private final String username = "root", password = "root";
+
     public Server() throws IOException, TimeoutException {
+        // Init RabitMQ
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         connection = factory.newConnection();
         channel = connection.createChannel();
+        // Init InfluxDB
+        initDB();
     }
 
     public static void main(String[] args) {
         try {
             Server server = new Server();
-            server.call();
+            server.startListener();
         } catch (IOException | TimeoutException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
 
-    private void call() throws IOException {
+    private void startListener() throws IOException {
         channel.exchangeDeclare(EXCHANGE_NAME, "topic");
         String routingKey = "edge.#";
         String queueName = channel.queueDeclare().getQueue();
@@ -46,16 +53,40 @@ public class Server implements AutoCloseable{
         DeliverCallback callback = (consumerTag, delivery) -> {
             String message = new String (delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
-            storeDB(message);
+            writeDB(message);
         };
         channel.basicConsume(queueName, true, callback, consumerTag -> { });
     }
 
-    private void storeDB(String message) {
+    private void writeDB(String message) {
+        String measurementName = "edge1";
+
+        // Write points to InfluxDB.
+        influxDB.write(Point.measurement(measurementName)
+            .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+            .tag("device", "voltmeter1")
+            .addField("value", Float.valueOf(message))
+            .build());
+
+        // Wait a few seconds in order to let the InfluxDB client
+        // write your points asynchronously (note: you can adjust the
+        // internal time interval if you need via 'enableBatch' call).
+        try {
+            Thread.sleep(5_000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Query your data using InfluxQL.
+        // https://docs.influxdata.com/influxdb/v1.7/query_language/data_exploration/#the-basic-select-statement
+        QueryResult queryResult = influxDB.query(new Query("SELECT * FROM " + measurementName));
+        System.out.println(queryResult);
+    }
+
+    private void initDB() {
         // Create an object to handle the communication with InfluxDB.
         // (best practice tip: reuse the 'influxDB' instance when possible)
-        final String serverURL = "http://127.0.0.1:8086", username = "root", password = "root";
-        final InfluxDB influxDB = InfluxDBFactory.connect(serverURL, username, password);
+        influxDB = InfluxDBFactory.connect(serverURL, username, password);
 
         // Create a database...
         // https://docs.influxdata.com/influxdb/v1.7/query_language/database_management/
@@ -82,37 +113,6 @@ public class Server implements AutoCloseable{
 
         // Close it if your application is terminating or you are not using it anymore.
         Runtime.getRuntime().addShutdownHook(new Thread(influxDB::close));
-
-        String measurementName = "edge1";
-        // Write points to InfluxDB.
-        influxDB.write(Point.measurement(measurementName)
-            .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
-            .tag("device", "voltmeter1")
-            .addField("value", Float.valueOf(message))
-            .build());
-
-
-        // Wait a few seconds in order to let the InfluxDB client
-        // write your points asynchronously (note: you can adjust the
-        // internal time interval if you need via 'enableBatch' call).
-        try {
-            Thread.sleep(5_000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        // Query your data using InfluxQL.
-        // https://docs.influxdata.com/influxdb/v1.7/query_language/data_exploration/#the-basic-select-statement
-        QueryResult queryResult = influxDB.query(new Query("SELECT * FROM " + measurementName));
-
-        System.out.println(queryResult);
-        // It will print something like:
-        // QueryResult [results=[Result [series=[Series [name=h2o_feet, tags=null,
-        //      columns=[time, level description, location, water_level],
-        //      values=[
-        //         [2020-03-22T20:50:12.929Z, below 3 feet, santa_monica, 2.064],
-        //         [2020-03-22T20:50:12.929Z, between 6 and 9 feet, coyote_creek, 8.12]
-        //      ]]], error=null]], error=null]
     }
 
     @Override
