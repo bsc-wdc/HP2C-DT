81908175b8423fc9a8b3391949e171d26daf79fb
iraola <eduiraola@gmail.com>
2023-11-08 15:24:46 +0100

    Add documentation to Server Java code

--- components/server/src/main/java/es/bsc/server/Server.java
+++ components/server/src/main/java/es/bsc/server/Server.java
@@ -15,7 +15,11 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-public class Server implements AutoCloseable{
+/**
+ * Implementation of the server logic interacting with an InfluxDB database and
+ * with edge devices via AMQP.
+ */
+public class Server implements AutoCloseable {
     private final Connection connection;
     private Channel channel;
     private InfluxDB influxDB;
@@ -42,6 +46,12 @@ public class Server implements AutoCloseable{
         }
     }
 
+    /**
+     * Start RabbitMQ consumer.
+     * The method will run indefinitely thanks to a DeliverCallback that calls
+     * writeDB to process each message received.
+     * Currently, receiving messages published to any "edge.#" topic.
+     */
     private void startListener() throws IOException {
         channel.exchangeDeclare(EXCHANGE_NAME, "topic");
         String routingKey = "edge.#";
@@ -60,6 +70,14 @@ public class Server implements AutoCloseable{
         channel.basicConsume(queueName, true, callback, consumerTag -> { });
     }
 
+    /**
+     * Write to Influx database.
+     * Use the second field of the routing key (EDGE_ID) as the `measurement`
+     * (time series) and the third field (DEVICE_ID) as the `tag` value.
+     * @param message Message of a measurement in string format.
+     * @param timestamp Message timestamp in long integer format.
+     * @param routingKey Routing key/topic the message was published to.
+     */
     private void writeDB(String message, long timestamp, String routingKey) {
         // Get measurement and tag of the edge measurement. Use "\\" to scape special regex character
         //   Example: routingKey = "edge.edge8080.voltmeter1"
@@ -74,34 +92,32 @@ public class Server implements AutoCloseable{
             .addField("value", Float.valueOf(message))
             .build());
 
-        // Wait a few seconds in order to let the InfluxDB client
-        // write your points asynchronously (note: you can adjust the
-        // internal time interval if you need via 'enableBatch' call).
+        // Wait a few seconds in order to let the InfluxDB client write your points asynchronously (note: you can adjust
+        // the internal time interval if you need via 'enableBatch' call). Just for testing to read DB after writing
         try {
             Thread.sleep(5L);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        // Query your data using InfluxQL.
-        // https://docs.influxdata.com/influxdb/v1.7/query_language/data_exploration/#the-basic-select-statement
+        // Query data using InfluxQL for testing
         QueryResult queryResult = influxDB.query(new Query("SELECT * FROM \"" + measurementName + "\""));
         System.out.println(queryResult);
     }
 
+    /**
+     * Initializes the "hp2cdt" InfluxDB database instance.
+     */
     private void initDB() {
         // Create an object to handle the communication with InfluxDB.
-        // (best practice tip: reuse the 'influxDB' instance when possible)
         influxDB = InfluxDBFactory.connect(serverURL, username, password);
 
-        // Create a database...
-        // https://docs.influxdata.com/influxdb/v1.7/query_language/database_management/
+        // Create a database if it does not already exist
         String databaseName = "hp2cdt";
         influxDB.query(new Query("CREATE DATABASE " + databaseName));
         influxDB.setDatabase(databaseName);
 
-        // ... and a retention policy, if necessary.
-        // https://docs.influxdata.com/influxdb/v1.7/query_language/database_management/
+        // Set up retention policy
         String retentionPolicyName = "one_day_only";
         influxDB.query(new Query("CREATE RETENTION POLICY " + retentionPolicyName
                 + " ON " + databaseName + " DURATION 1d REPLICATION 1 DEFAULT"));
@@ -117,7 +133,7 @@ public class Server implements AutoCloseable{
                 })
         );
 
-        // Close it if your application is terminating or you are not using it anymore.
+        // Close when application terminates.
         Runtime.getRuntime().addShutdownHook(new Thread(influxDB::close));
     }
 
