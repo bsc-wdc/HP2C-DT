8f96d44bbfd69de0e2d9ccaec1b932c772623c08
mgarci5 <mgarci5@bsc.es>
2023-11-24 12:48:40 +0100

    Accept multiple connections in OpalServer

--- components/opalSimulator/src/main/java/es/bsc/hp2c/opalSimulator/ServerSimulator.java
+++ components/opalSimulator/src/main/java/es/bsc/hp2c/opalSimulator/ServerSimulator.java
@@ -1,7 +1,6 @@
 package es.bsc.hp2c.opalSimulator;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -9,25 +8,40 @@ import java.net.Socket;
 import java.nio.ByteBuffer;
 
 public class ServerSimulator {
-
     private static ServerSocket server;
     private static int port = 31002;
 
     public static void main(String[] args) throws IOException {
         server = new ServerSocket(port, 0, InetAddress.getByName("0.0.0.0"));
 
-        System.out.println("Waiting for the client request");
-        Socket socket = server.accept();
+        System.out.println("Server running. Waiting for client requests...");
+
         while (true) {
-            DataInputStream dis = new DataInputStream(socket.getInputStream());
-            byte[] buffer = new byte[4 * Float.BYTES];
-            dis.readFully(buffer);
+            Socket clientSocket = server.accept();
+            System.out.println("Accepted connection from: " + clientSocket.getInetAddress().getHostAddress());
 
-            ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
+            Thread clientHandlerThread = new Thread(() -> handleClient(clientSocket));
+            clientHandlerThread.start();
+        }
+    }
 
-            while (byteBuffer.remaining() > 0) {
-                System.out.println("Message Received: " + byteBuffer.getFloat());
+    private static void handleClient(Socket clientSocket) {
+        try {
+            DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+
+            while (true) {
+                byte[] buffer = new byte[4 * Float.BYTES];
+                dis.readFully(buffer);
+
+                ByteBuffer byteBuffer = ByteBuffer.wrap(buffer);
+
+                while (byteBuffer.remaining() > 0) {
+                    System.out.println("Message Received from " + clientSocket.getInetAddress().getHostAddress() +
+                            ": " + byteBuffer.getFloat());
+                }
             }
+        } catch (IOException e) {
+            System.err.println("Error handling client: " + e.getMessage());
         }
     }
 }
