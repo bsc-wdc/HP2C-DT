197a9f2b761300897a73ac421dde09b787f90671
iraola <eduiraola@gmail.com>
2023-12-11 12:24:47 +0100

    Fix CommUtils methods

--- components/edge/src/main/java/es/bsc/hp2c/edge/utils/CommUtils.java
+++ components/edge/src/main/java/es/bsc/hp2c/edge/utils/CommUtils.java
@@ -1,6 +1,6 @@
 package es.bsc.hp2c.edge.utils;
 
-import java.io.*;
+import java.nio.ByteBuffer;
 
 /**
  * Utility class for commonly used methods related to communications
@@ -9,27 +9,21 @@ public final class CommUtils {
     private CommUtils(){}
 
     public static byte[] FloatArrayToBytes(Float[] values) {
-        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
-            ObjectOutputStream out = null;
-            out = new ObjectOutputStream(bos);
-            out.writeObject(values);
-            out.flush();
-            return bos.toByteArray();
-        } catch (IOException e) {
-            System.err.println("Error converting Float[] to bytes[]");
-            throw new RuntimeException(e);
+        int nFloat = values.length;
+        ByteBuffer byteBuffer = ByteBuffer.allocate(nFloat * Float.BYTES);
+        for (Float value : values) {
+            byteBuffer.putFloat(value);
         }
+        return byteBuffer.array();
     }
 
     public static Float[] BytesToFloatArray(byte[] messageBytes) {
-        ByteArrayInputStream bis = new ByteArrayInputStream(messageBytes);
-        try (ObjectInput in = new ObjectInputStream(bis)) {
-            ObjectInputStream obj = (ObjectInputStream) in.readObject();
-            return (Float[]) obj.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Error converting bytes[] to Float[]");
-            throw new RuntimeException(e);
+        ByteBuffer byteBuffer = ByteBuffer.wrap(messageBytes);
+        int nFloat = messageBytes.length / Float.BYTES;
+        Float[] messageValues = new Float[nFloat];
+        for (int i = 0; i < nFloat; i++) {
+            messageValues[i] = byteBuffer.getFloat();
         }
+        return messageValues;
     }
-
 }
