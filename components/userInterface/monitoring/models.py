28897a1352edcfa44db977af1b1227cff0f680a7
MauroGarciaLorenzo <mgarci5@bsc.es>
2024-02-22 20:31:51 +0100

    Implement monitoring (without frontend)

--- components/userInterface/monitoring/models.py
+++ components/userInterface/monitoring/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Deployment(models.Model):
     name = models.CharField(max_length=100)
+    uid = models.CharField(max_length=100, default="")
     def __str__(self):
         return self.name
 
--- components/userInterface/scripts/get_dashboards.sh
+++ components/userInterface/scripts/get_dashboards.sh
@@ -3,6 +3,17 @@ export GRAFANA_API_KEY="glsa_MMyee1K5yzSOFEOQ0Rcx5llirdHt5Jle_6b10b6a4"
 export GRAFHOST="http://localhost:3000"
 
 mkdir -p ${ROOT_DIR}/dashboards
+
 for uid in $(curl -sk -H "Authorization: Bearer ${GRAFANA_API_KEY}" ${GRAFHOST}/api/search | jq '.[].uid' -r); do 
-    curl -sk -H "Authorization: Bearer ${GRAFANA_API_KEY}" ${GRAFHOST}/api/dashboards/uid/$uid | jq . > ${ROOT_DIR}/dashboards/$uid.json 
+    dashboard_info=$(curl -sk -H "Authorization: Bearer ${GRAFANA_API_KEY}" ${GRAFHOST}/api/dashboards/uid/$uid)
+    title=$(echo $dashboard_info | jq -r '.dashboard.title')
+
+    if echo "$title" | grep -q "HP2CDT - "; then
+        name_of_deployment=$(echo "$title" | awk -F "HP2CDT - " '{print $2}' | tr -d '[:space:]')
+    else
+        name_of_deployment="$title"
+    fi
+
+    echo "Exporting dashboard with UID: $uid to file: $name_of_deployment.json"
+    echo $dashboard_info | jq . > ${ROOT_DIR}/dashboards/$name_of_deployment.json
 done
