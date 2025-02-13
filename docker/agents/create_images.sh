a0e4e1b5299e848be865052f77d9b37fb7d1df44
iraola <eduardo.iraola@bsc.es>
2025-02-13 18:12:39 +0100

    Modify edge/agent images to allow local COMPSs devel
    Files are copied from the local /opt/COMPSs dir and compss/compss:trunk
    image.

--- docker/agents/create_images.sh
+++ docker/agents/create_images.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 echo  "Creating AGENTS images"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Argument parsing
 if [[ "${#}" -lt "1" ]]; then
     COMPSS_VERSION="trunk"
 else 
     COMPSS_VERSION="${1}"
 fi
-  
 
+# Check version to use a default or custom COMPSs source
+if [ "$COMPSS_VERSION" = "trunk" ]; then
+    # Use local installation to copy local COMPSs files
+    echo "WARNING: using local COMPSs install to build agents image"
+    echo "Make sure compss/compss:$COMPSS_VERSION image is built locally!"
+    rsync -cra /opt/COMPSs/ ${SCRIPT_DIR}/COMPSs/
+    DOCKERFILE="Dockerfile.agents.DEVEL"
+else
+    echo "Using official COMPSs image"
+    DOCKERFILE="Dockerfile.agents"
+fi
 
 # Setting up trap to clear environment
 on_exit(){
+    # Delete temporary files
     if [ -n "${tmpfile}" ]; then
         rm "${tmpfile}"
+    fi
+    if [ "$COMPSS_VERSION" = "trunk" ]; then
+        echo "REMOVING TEMP DIR!"
+        rm -r ${SCRIPT_DIR}/COMPSs/
     fi
 }
 trap 'on_exit' EXIT
@@ -24,7 +41,7 @@ services:
   agents_manager:
     build:
       context: .
-      dockerfile: Dockerfile.agents
+      dockerfile: ${DOCKERFILE}
       target: agents_manager
       args:
         COMPSS_VERSION: ${COMPSS_VERSION}
@@ -32,7 +49,7 @@ services:
   compss-agent-java:
     build:
       context: .
-      dockerfile: Dockerfile.agents
+      dockerfile: ${DOCKERFILE}
       target: java_agent
       args:
         COMPSS_VERSION: ${COMPSS_VERSION}
@@ -40,7 +57,7 @@ services:
   compss-agent-python:
     build:
       context: .
-      dockerfile: Dockerfile.agents
+      dockerfile: ${DOCKERFILE}
       target: python_agent
       args:
         COMPSS_VERSION: ${COMPSS_VERSION}
