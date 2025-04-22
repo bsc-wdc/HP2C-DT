6619cc10c989ce3ec38c96a1345c8945cdf69227
mgarci5 <mgarci5@bsc.es>
2025-04-22 13:35:15 +0200

    Add new ip to deploy_server

--- deployments/deploy_server.sh
+++ deployments/deploy_server.sh
@@ -131,6 +131,9 @@ fi
 if [ -z "$ip_address" ]; then
     ip_address=$(ip addr show | grep -E 'inet\s' | grep -E 'enxcc[0-9]+' | awk '{print $2}' | cut -d '/' -f 1 | head -n 1)
 fi
+if [ -z "$ip_address" ]; then
+    ip_address=$(ip addr show | grep -E 'inet\s' | grep -E 'ens[0-9]+' | awk '{print $2}' | cut -d '/' -f 1 | head -n 1)
+fi
 
 custom_ip_address="172.29.128.1"
 
