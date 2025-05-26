fc0dabef7708bea5b95e766b81a8e8c040ea3542
mgarci5 <mgarci5@bsc.es>
2025-05-26 11:37:03 +0200

    Use files por json static content

--- components/common/src/test/java/es/bsc/hp2c/common/funcs/FuncTest.java
+++ components/common/src/test/java/es/bsc/hp2c/common/funcs/FuncTest.java
@@ -1,69 +1,52 @@
 package es.bsc.hp2c.common.funcs;
 
 import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
 
 import static es.bsc.hp2c.common.funcs.Func.transformFuncToServerFormat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransformUtilsTest {
 
-    @org.junit.jupiter.api.Test
-    void testBasicTransformation() {
-        JSONObject func = new JSONObject(
-                "{" +
-                        "\"label\": \"VoltLimitation\"," +
-                        "\"lang\": \"Python\"," +
-                        "\"parameters\": {" +
-                        "\"sensors\": [\"Voltmeter Gen1\"]," +
-                        "\"actuators\": [\"Three-Phase Switch Gen1\"]," +
-                        "\"other\": {}" +
-                        "}," +
-                        "\"trigger\": {" +
-                        "\"type\": \"onRead\"," +
-                        "\"parameters\": {" +
-                        "\"trigger-sensor\": [\"Voltmeter Gen1\"]," +
-                        "\"interval\": 1" +
-                        "}" +
-                        "}" +
-                        "}"
-        );
+    private JSONObject loadJSON(String filename) throws IOException {
+        String resourcePath = "jsons/" + filename;
+        InputStream is = getClass().getClassLoader().getResourceAsStream(resourcePath);
+        if (is == null) {
+            throw new FileNotFoundException("Resource not found: " + resourcePath);
+        }
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder content = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            content.append(line);
+        }
+        reader.close();
+
+        return new JSONObject(content.toString());
+    }
+
+
+
+    @Test
+    void testBasicTransformation() throws IOException {
+        JSONObject func = loadJSON("basic_transformation.json");
         transformFuncToServerFormat(func, "edge1");
 
-        JSONObject params = func.getJSONObject("parameters");
-        JSONObject sensors = params.getJSONObject("sensors");
-        JSONObject actuators = params.getJSONObject("actuators");
-        JSONObject triggerSensors = func.getJSONObject("trigger").getJSONObject("parameters")
-                .getJSONObject("trigger-sensor");
+        JSONObject sensors = func.getJSONObject("parameters").getJSONObject("sensors");
+        JSONObject actuators = func.getJSONObject("parameters").getJSONObject("actuators");
+        JSONObject triggerSensors = func.getJSONObject("trigger").getJSONObject("parameters").getJSONObject("trigger-sensor");
 
-        // Assert that every noted device is within "edge1"
         assertEquals("Voltmeter Gen1", sensors.getJSONArray("edge1").getString(0));
         assertEquals("Three-Phase Switch Gen1", actuators.getJSONArray("edge1").getString(0));
         assertEquals("Voltmeter Gen1", triggerSensors.getJSONArray("edge1").getString(0));
     }
 
-    @org.junit.jupiter.api.Test
-    void testEmptyActuatorsArray() {
-        JSONObject func = new JSONObject(
-                "{" +
-                        "\"label\": \"CalcPower\"," +
-                        "\"lang\": \"Java\"," +
-                        "\"type\": \"workflow\"," +
-                        "\"method-name\": \"es.bsc.hp2c.edge.funcs.CalcPower\"," +
-                        "\"parameters\": {" +
-                        "\"sensors\": [\"Voltmeter Gen1\", \"Ammeter Gen1\"]," +
-                        "\"actuators\": []," +
-                        "\"other\": {}" +
-                        "}," +
-                        "\"trigger\": {" +
-                        "\"type\": \"onFrequency\"," +
-                        "\"parameters\": {" +
-                        "\"frequency\": 2000" +
-                        "}" +
-                        "}" +
-                        "}"
-        );
-
+    @Test
+    void testEmptyActuatorsArray() throws IOException {
+        JSONObject func = loadJSON("empty_actuators.json");
         transformFuncToServerFormat(func, "edge2");
 
         JSONObject sensors = func.getJSONObject("parameters").getJSONObject("sensors");
@@ -73,24 +56,9 @@ class TransformUtilsTest {
         assertEquals(0, actuators.getJSONArray("edge2").length());
     }
 
-
-    @org.junit.jupiter.api.Test
-    void testInvalidTriggerSensorType() {
-        JSONObject func = new JSONObject(
-                "{" +
-                        "\"label\": \"FaultyFunc\"," +
-                        "\"parameters\": {" +
-                        "\"sensors\": [\"Sensor1\"]," +
-                        "\"actuators\": [\"Actuator1\"]" +
-                        "}," +
-                        "\"trigger\": {" +
-                        "\"type\": \"onRead\"," +
-                        "\"parameters\": {" +
-                        "\"trigger-sensor\": \"Not an array\"" +
-                        "}" +
-                        "}" +
-                        "}"
-        );
+    @Test
+    void testInvalidTriggerSensorType() throws IOException {
+        JSONObject func = loadJSON("invalid_trigger_sensor.json");
 
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> transformFuncToServerFormat(func, "edge3"));
@@ -98,29 +66,13 @@ class TransformUtilsTest {
         assertTrue(exception.getMessage().contains("Expected 'trigger-sensor' to be a JSONArray"));
     }
 
-    @org.junit.jupiter.api.Test
-    void testInvalidSensorArray() {
-        JSONObject func = new JSONObject(
-                "{" +
-                        "\"label\": \"EmptySensors\"," +
-                        "\"lang\": \"Java\"," +
-                        "\"parameters\": {" +
-                        "\"sensors\": \"Not an array\"," +
-                        "\"actuators\": [\"Actuator1\"]" +
-                        "}," +
-                        "\"trigger\": {" +
-                        "\"type\": \"onRead\"," +
-                        "\"parameters\": {" +
-                        "\"trigger-sensor\": [\"Sensor1\"]" +
-                        "}" +
-                        "}" +
-                        "}"
-        );
+    @Test
+    void testInvalidSensorArray() throws IOException {
+        JSONObject func = loadJSON("invalid_sensor_array.json");
 
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> transformFuncToServerFormat(func, "edge4"));
 
         assertTrue(exception.getMessage().contains("Expected sensors to be a JSONArray"));
     }
-
 }
